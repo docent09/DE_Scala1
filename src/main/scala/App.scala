873601170c1d9e970f41084f501249fd888a9e54
docent09 <vfdekan@gmail.com>
2022-11-01 14:03:30 +0400

    Commit #3

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -19,8 +19,9 @@ object App {
       val tax = 0.13  // налог
       (year_income * (1 + prize / 100) + eating) * (1 - tax) / 12
     }
-    println("Вычисляем месячный оклад:")
-    println(salary(600, 10, 60))
+    println("Вычисляем месячный оклад сотрудника:")
+    val new_employee_month_salary = salary(600, 10, 60)
+    println(new_employee_month_salary)
 
 
     /* Задание с.
@@ -32,16 +33,18 @@ object App {
     */
     def salary_deviation(salary: Double, salary_list: List[Double]): Double = {
       var sum_salary_list = 0: Double
+      // Находим сумму элементов списка
       for (n <- salary_list) {
         sum_salary_list = sum_salary_list + n
       }
-      val mean_salary_list = sum_salary_list / salary_list.size: Double // средний оклад по отделу
+      // средний оклад по отделу
+      val mean_salary_list = sum_salary_list / salary_list.size: Double
       (salary - mean_salary_list) / mean_salary_list * 100
     }
-    val salary_list: List[Double] = List.apply(100, 150, 200, 80, 120, 75)
-    val salary1 = salary(600, 10, 60): Double
-    println("Отклонение оклада от среднего по отделу:")
-    println(salary_deviation(salary1, salary_list), "%")
+    val salary_list: List[Double] = List.apply(100, 150, 200, 80, 120, 75)  // оклады сотрудников
+    println("Отклонение оклада нового сотрудника от среднего по отделу:")
+    val new_employee_month_salary_deviation = salary_deviation(new_employee_month_salary, salary_list)
+    println(new_employee_month_salary_deviation, "%")
 
 
     /* Задание d.
@@ -49,7 +52,7 @@ object App {
     необходимую сумму с учетом результатов прошлого задания.
     Добавьте его зарплату в список и вычислите значение самой высокой зарплаты и самой низкой.
     */
-    def salary_change(salary: Double, delta: Double): Double = salary + delta
+    def salary_change(salary: Double, delta_percent: Double): Double = salary * (1 + delta_percent/100)
 
     def min_max_salary(salary_list: List[Double]): List[Double] = {
       var min = 1000000: Double
@@ -60,6 +63,17 @@ object App {
       }
       List.apply(min, max)
     }
+
+    def add_to_list_salaries(salary_list: List[Double], new_element: Double): List[Double] = {
+      salary_list :+ new_element
+    }
+    // Рассчитываем новую зарплату нового сотрудника
+    val new_new_employee_month_salary = salary_change(new_employee_month_salary, new_employee_month_salary_deviation)
+    // Добавляем новую зарплату нового сотрудника в список зарплат отдела
+    val new_salary_list = add_to_list_salaries(salary_list, new_new_employee_month_salary)
+    // Печатаем минимальную и максимальную зарплаты сотрудников отдела
+    println("Минимальная и максимальная зарплаты сотрудников отдела:")
+    println(min_max_salary(new_salary_list))
 
 
     /* Задание e.
